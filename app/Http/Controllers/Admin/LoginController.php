f963595ca057c4795d3a9ac8679a67e565fc3e05
asepher <aseph4@gmail.com>
2023-06-09 12:00:40 +0700

    Update file controller

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public  function AdminLogin()
+    {
+        dd('login page AdminLogin');
+    }
+
+    public  function AdminRegister()
+    {
+        dd('login page AdminRegister');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('login', [LoginController::class, 'AdminLogin'])->name('admin.login');
+Route::get('admin/register',[RegisterController::class, 'AdminRegister'])->name('admin.register');
